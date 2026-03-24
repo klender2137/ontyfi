7d7e6aafe930b84c6addd4579a68ab59c8c82d0e
andriiplotnykov-alt <andriiplotnykov@gmail.com>
2026-03-24 22:08:20 +0100

    Fix vite root to public for northflank build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
+  
+  root: 'public', 
   build: {
-
+  
     outDir: '../dist', 
     emptyOutDir: true,
   },
